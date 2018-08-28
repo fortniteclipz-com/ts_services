32f671fc3e812f4e0fb5fa3b7c9e29db4ba32abd
Sachin Ahuja <sachinahj@gmail.com>
2018-08-28 01:57:45 -0700

    postprocess

--- modules/media_export__postprocess/run.py
+++ modules/media_export__postprocess/run.py
@@ -1,5 +1,6 @@
+import ts_aws.dynamodb.clip
+import ts_aws.dynamodb.montage
 import ts_logger
-import ts_model.Exception
 import ts_model.Status
 
 import json
@@ -10,12 +11,27 @@ logger = ts_logger.get(__name__)
 def run(event, context):
     try:
         logger.info("start", event=event, context=context)
+        status = event['detail']['status']
+        media_type = event['detail']['userMetadata']['media_type']
+        media_id = event['detail']['userMetadata']['media_id']
+
+        if media_type == "clip":
+            media = ts_aws.dynamodb.clip.get_clip(media_id)
+        elif media_type == "montage":
+            media = ts_aws.dynamodb.montage.get_montage(media_id)
+
+        if status == "COMPLETE":
+            media._status_export = ts_model.Status.READY
+        else:
+            media._status_export = ts_model.Status.NONE
+
+        if media_type == "clip":
+            ts_aws.dynamodb.clip.save_clip(media)
+        elif media_type == "montage":
+            ts_aws.dynamodb.montage.save_montage(media)
+
         logger.info("success")
         return True
-
-    # except ts_model.Exception as e:
-    #     logger.error("error", _module=f"{e.__class__.__module__}", _class=f"{e.__class__.__name__}", _message=str(e), traceback=''.join(traceback.format_exc()))
-    #     pass
 
     except Exception as e:
         logger.error("error", _module=f"{e.__class__.__module__}", _class=f"{e.__class__.__name__}", _message=str(e), traceback=''.join(traceback.format_exc()))
--- modules/media_export__postprocess/test.py
+++ modules/media_export__postprocess/test.py
@@ -1,8 +1,66 @@
 import run
 import json
 
-bodies = [{}]
+events = [{
+    'version': '0',
+    'id': '1bd45bb8-c272-c719-249b-41faf90ca6e5',
+    'detail-type': 'MediaConvert Job State Change',
+    'source': 'aws.mediaconvert',
+    'account': '589344262905',
+    'time': '2018-08-28T08:32:40Z',
+    'region': 'us-west-1',
+    'resources': ['arn:aws:mediaconvert:us-west-1:589344262905:jobs/1535445152005-uwqnsy'],
+    'detail':
+    {
+        'timestamp': 1535445160581,
+        'accountId': '589344262905',
+        'queue': 'arn:aws:mediaconvert:us-west-1:589344262905:queues/Default',
+        'jobId': '1535445152005-uwqnsy',
+        'status': 'COMPLETE',
+        'userMetadata':
+        {
+            'media_type': 'clip',
+            'media_id': 'c-zbFvt88cty9VhkpKqHVLDo'
+        },
+        'outputGroupDetails': [
+        {
+            'outputDetails': [
+            {
+                'durationInMs': 6673,
+                'videoDetails':
+                {
+                    'widthInPx': 1280,
+                    'heightInPx': 720
+                }
+            }],
+            'type': 'FILE_GROUP'
+        }]
+    }
+}, {
+    'version': '0',
+    'id': 'f7ebd71a-74fa-db35-dc21-333d9c22452c',
+    'detail-type': 'MediaConvert Job State Change',
+    'source': 'aws.mediaconvert',
+    'account': '589344262905',
+    'time': '2018-08-28T08:36:47Z',
+    'region': 'us-west-1',
+    'resources': ['arn:aws:mediaconvert:us-west-1:589344262905:jobs/1535445400397-i70asm'],
+    'detail':
+    {
+        'timestamp': 1535445407020,
+        'accountId': '589344262905',
+        'queue': 'arn:aws:mediaconvert:us-west-1:589344262905:queues/Default',
+        'jobId': '1535445400397-i70asm',
+        'status': 'ERROR',
+        'errorCode': 1404,
+        'errorMessage': 'Unable to open input file [s3://twitch-stitch-main/clips/c-zbFvt88cty9VhkpKqHVLDo/playlist-video.m3u8]: [Failed probe/open: [HLS reader failed to create HLS manifest: Failed to read data: Input not found at s3://twitch-stitch-main/clips/c-zbFvt88cty9VhkpKqHVLDo/playlist-video.m3u8]]',
+        'userMetadata':
+        {
+            'media_type': 'clip',
+            'media_id': 'c-zbFvt88cty9VhkpKqHVLDo'
+        }
+    }
+}]
 
-for body in bodies:
-    event = {'Records': [{'body': json.dumps(body)}]}
+for event in events:
     run.run(event, {})
