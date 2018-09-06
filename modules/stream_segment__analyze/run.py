4e1fb613c68e540f4c9c83d2e7e0a63c97905cce
Sachin Ahuja <sachinahj@gmail.com>
2018-09-06 00:31:24 -0700

    bug fixes

--- modules/stream_segment__analyze/run.py
+++ modules/stream_segment__analyze/run.py
@@ -3,6 +3,7 @@ import ts_aws.dynamodb.stream_segment
 import ts_aws.s3
 import ts_aws.sqs.stream__initialize
 import ts_aws.sqs.stream_segment__analyze
+import ts_aws.sqs.stream_segment__download
 import ts_file
 import ts_logger
 import ts_media
@@ -48,13 +49,13 @@ def run(event, context):
         # get stream_segment
         ss = ts_aws.dynamodb.stream_segment.get_stream_segment(stream_id, segment)
 
-        if ss._status_analyzed == ts_model.Status.READY:
+        if ss._status_analyze == ts_model.Status.READY:
             raise ts_model.Exception(ts_model.Exception.STREAM_SEGMENT__ALREADY_ANALYZED)
 
         if ss._status_download == ts_model.Status.NONE:
             ss._status_download = ts_model.Status.INITIALIZING
-            ts_aws.dynamodb.stream.save_stream_segment(ss)
-            ts_aws.sqs.stream__download.send_message({
+            ts_aws.dynamodb.stream_segment.save_stream_segment(ss)
+            ts_aws.sqs.stream_segment__download.send_message({
                 'stream_id': ss.stream_id,
                 'segment': ss.segment,
             })
@@ -73,6 +74,11 @@ def run(event, context):
             ts_aws.s3.upload_file_thumbnails(thumbnail_filename, thumbnail_key)
             os.remove(thumbnail_filename)
 
+        # save stream_segment
+        ss._status_analyze = ts_model.Status.READY
+        ts_aws.dynamodb.stream_segment.save_stream_segment(ss)
+
+        # clean up
         ts_file.delete(media_filename)
 
         logger.info("success")
