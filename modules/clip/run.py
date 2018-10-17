d82fcb6e5e94c1949389a9980c42d58c5a3e01af
Sachin Ahuja <sachinahj@gmail.com>
2018-10-17 11:28:16 -0700

    stream time in updates

--- modules/clip/run.py
+++ modules/clip/run.py
@@ -105,16 +105,16 @@ def run(event, context):
             if is_last_cs and int(round(css.stream_time_in)) == int(round(clip.time_out)):
                 continue
 
-            time_in = clip.time_in - css.stream_time_in  if is_first_cs else None
-            time_out = clip.time_out - css.stream_time_in if is_last_cs else None
+            segment_time_in = clip.time_in - css.stream_time_in  if is_first_cs else None
+            segment_time_out = clip.time_out - css.stream_time_in if is_last_cs else None
 
             cs = ts_model.ClipSegment(
                 clip_id=clip.clip_id,
                 segment=css.segment,
                 stream_id=clip.stream_id,
                 media_key=css.media_key,
-                segment_time_in=time_in,
-                segment_time_out=time_out,
+                segment_time_in=segment_time_in,
+                segment_time_out=segment_time_out,
             )
             clip_segments.append(cs)
 
